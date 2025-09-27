35d4368808b327a6d4f6b599ab0ab9a38a77c827
Antoine Moreau <antoine9510@gmail.com>
2025-09-27 21:04:24 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					content: { type: 'text', value: ' 🔗' }
+				}
+			],
+		]
+	},
+});
