1f77733546c37f4724b37751fc5f7931fa87c016
Antoine Moreau <antoine9510@gmail.com>
2026-06-10 10:40:12 +0200

    test id generation

--- src/content.config.ts
+++ src/content.config.ts
@@ -18,29 +18,29 @@ const allPages = defineCollection({
 			draft: z.boolean().optional(),
 		}),
 });
-const jdr = defineCollection({
-	loader: glob({ base: './src/content/jdr', pattern: '*.{md,mdx}' }),
-	schema: ({}) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			tags: z.array(z.string()),
-			draft: z.boolean().optional(),
-		}),
-});
-const politique = defineCollection({
-	loader: glob({ base: './src/content/politique', pattern: '*.{md,mdx}' }),
-	schema: ({}) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			tags: z.array(z.string()),
-			draft: z.boolean().optional(),
-		}),
-});
+// const jdr = defineCollection({
+// 	loader: glob({ base: './src/content/jdr', pattern: '*.{md,mdx}' }),
+// 	schema: ({}) =>
+// 		z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			pubDate: z.coerce.date(),
+// 			updatedDate: z.coerce.date().optional(),
+// 			tags: z.array(z.string()),
+// 			draft: z.boolean().optional(),
+// 		}),
+// });
+// const politique = defineCollection({
+// 	loader: glob({ base: './src/content/politique', pattern: '*.{md,mdx}' }),
+// 	schema: ({}) =>
+// 		z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			pubDate: z.coerce.date(),
+// 			updatedDate: z.coerce.date().optional(),
+// 			tags: z.array(z.string()),
+// 			draft: z.boolean().optional(),
+// 		}),
+// });
 
-export const collections = { allPages, jdr, politique };
+export const collections = { allPages/*, jdr, politique*/ };
