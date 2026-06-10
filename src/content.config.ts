6acdaadad210aa9aceec4c8281a96deec9767ef6
Antoine Moreau <antoine9510@gmail.com>
2026-06-10 09:53:22 +0200

    deh

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,18 +2,18 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
-// const allPages = defineCollection({
-// 	loader: glob({ base: './src/content/', pattern: '**/*.{md,mdx}' }),
-// 	schema: ({}) =>
-// 		z.object({
-// 			title: z.string(),
-// 			description: z.string(),
-// 			pubDate: z.coerce.date(),
-// 			updatedDate: z.coerce.date().optional(),
-// 			tags: z.array(z.string()),
-// 			draft: z.boolean().optional(),
-// 		}),
-// });
+const allPages = defineCollection({
+	loader: glob({ base: './src/content/', pattern: '**/*.{md,mdx}' }),
+	schema: ({}) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			tags: z.array(z.string()),
+			draft: z.boolean().optional(),
+		}),
+});
 const jdr = defineCollection({
 	loader: glob({ base: './src/content/jdr/', pattern: '*.{md,mdx}' }),
 	schema: ({}) =>
