97bd57a219db2d26c40416e988bc0aeb98192da3
Antoine Moreau <antoine9510@gmail.com>
2026-06-10 11:18:51 +0200

    collections sans glob fin

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,28 @@
 import { defineCollection, z } from "astro:content";
-// import { glob } from "astro/loaders";
-// import { z } from "astro/zod";
+
+const politique = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		tags: z.array(z.string()),
+		draft: z.boolean().optional(),
+	}),
+})
+
+const jdr = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		tags: z.array(z.string()),
+		draft: z.boolean().optional(),
+	}),
+})
 
 const autres = defineCollection({
 	type: "content",
@@ -14,45 +36,4 @@ const autres = defineCollection({
 	}),
 })
 
-// const autres = defineCollection({
-// 	loader: glob({ 
-// 		base: './src/content', 
-// 		pattern: '**/*.{md,mdx}',
-// 		generateId: ({ entry }) => entry.replace(/\.md$/, ''),
-// 	}),
-// 	schema: ({}) =>
-// 		z.object({
-// 			title: z.string(),
-// 			description: z.string(),
-// 			pubDate: z.coerce.date(),
-// 			updatedDate: z.coerce.date().optional(),
-// 			tags: z.array(z.string()),
-// 			draft: z.boolean().optional(),
-// 		}),
-// });
-// const jdr = defineCollection({
-// 	loader: glob({ base: './src/content/jdr', pattern: '*.{md,mdx}' }),
-// 	schema: ({}) =>
-// 		z.object({
-// 			title: z.string(),
-// 			description: z.string(),
-// 			pubDate: z.coerce.date(),
-// 			updatedDate: z.coerce.date().optional(),
-// 			tags: z.array(z.string()),
-// 			draft: z.boolean().optional(),
-// 		}),
-// });
-// const politique = defineCollection({
-// 	loader: glob({ base: './src/content/politique', pattern: '*.{md,mdx}' }),
-// 	schema: ({}) =>
-// 		z.object({
-// 			title: z.string(),
-// 			description: z.string(),
-// 			pubDate: z.coerce.date(),
-// 			updatedDate: z.coerce.date().optional(),
-// 			tags: z.array(z.string()),
-// 			draft: z.boolean().optional(),
-// 		}),
-// });
-
-export const collections = { autres };
+export const collections = { jdr, politique, autres };
