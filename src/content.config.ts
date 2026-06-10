43d65502df71e999e84cc1e120b58ec3eaa39666
Antoine Moreau <antoine9510@gmail.com>
2026-06-10 11:15:00 +0200

    autres suites

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,17 @@ import { defineCollection, z } from "astro:content";
 // import { glob } from "astro/loaders";
 // import { z } from "astro/zod";
 
-// const content = defineCollection({
-// 	type: "content",
-// 	schema: z.object({
-// 		title: z.string(),
-// 		description: z.string(),
-// 		pubDate: z.coerce.date(),
-// 		updatedDate: z.coerce.date().optional(),
-// 		tags: z.array(z.string()),
-// 		draft: z.boolean().optional(),
-// 	}),
-// })
+const autres = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		tags: z.array(z.string()),
+		draft: z.boolean().optional(),
+	}),
+})
 
 // const autres = defineCollection({
 // 	loader: glob({ 
@@ -55,4 +55,4 @@ import { defineCollection, z } from "astro:content";
 // 		}),
 // });
 
-// export const collections = { content };
+export const collections = { autres };
