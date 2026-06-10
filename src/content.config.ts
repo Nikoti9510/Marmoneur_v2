7de9a8e689dec100fcaec9fbe5da07c25ddb7c92
Antoine Moreau <antoine9510@gmail.com>
2026-06-10 10:55:39 +0200

    test sans glob

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,23 +1,35 @@
-import { defineCollection } from "astro:content";
-import { glob } from "astro/loaders";
-import { z } from "astro/zod";
+import { defineCollection, z } from "astro:content";
+// import { glob } from "astro/loaders";
+// import { z } from "astro/zod";
 
-const allPages = defineCollection({
-	loader: glob({ 
-		base: './src/content', 
-		pattern: '**/*.{md,mdx}',
-		generateId: ({ entry }) => entry.replace(/\.md$/, ''),
+const content = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		tags: z.array(z.string()),
+		draft: z.boolean().optional(),
 	}),
-	schema: ({}) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			tags: z.array(z.string()),
-			draft: z.boolean().optional(),
-		}),
-});
+})
+
+// const allPages = defineCollection({
+// 	loader: glob({ 
+// 		base: './src/content', 
+// 		pattern: '**/*.{md,mdx}',
+// 		generateId: ({ entry }) => entry.replace(/\.md$/, ''),
+// 	}),
+// 	schema: ({}) =>
+// 		z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			pubDate: z.coerce.date(),
+// 			updatedDate: z.coerce.date().optional(),
+// 			tags: z.array(z.string()),
+// 			draft: z.boolean().optional(),
+// 		}),
+// });
 // const jdr = defineCollection({
 // 	loader: glob({ base: './src/content/jdr', pattern: '*.{md,mdx}' }),
 // 	schema: ({}) =>
---
+++ src/content/test.md
@@ -0,0 +1,9 @@
+---
+title: "test popolilititiqueque"
+description: "better a pig"
+pubDate: '2026-06-01'
+tags: ["politique"]
+draft: false
+---
+
+Couille
